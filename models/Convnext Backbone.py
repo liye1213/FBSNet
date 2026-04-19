40a627bc4baa4c5af943d787f4a6ac283ff6ed6a
liye1213 <1369629111@qq.com>
2026-04-19 16:02:11 +0800

    convnext backbone.py

--- models/Convnext Backbone.py
+++ models/Convnext Backbone.py
@@ -1,7 +1,7 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-from timm.models.layers import DropPath, trunc_normal_
+from timm.models.layers import DropPath, trunc_normal_ 
 from timm.models.registry import register_model
  
 
